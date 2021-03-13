3a9b296564f4ebb33cd1a76a34b12174b963c834
Kyoungjin Lee <44639709+black3279@users.noreply.github.com>
2021-03-13 16:51:19 +0900

    Add files via upload

---
+++ 2_Variable.py
@@ -0,0 +1,4 @@
+
+identify = '지구인'
+number_of_legs = 4
+print('안녕 ! 나는 ', identify, '이야. 나는 다리가 ', number_of_legs, '있어 !')
---
+++ 3_Syntax1.py
@@ -0,0 +1,26 @@
+
+# Python 주석은 이런 식으로 한다.
+
+"""
+여러줄은 이렇게 한다
+"""
+
+
+my_name = '이경진'
+my_age = 29
+next_age = my_age+1
+
+print('내 이름은', my_name, '이고 나이는 ', my_age, '살이며 내년에는', next_age , ' 살이다')
+
+my_age = '29'
+# 이건 불가능 next_age = my_age + 1
+
+print('내 이름은', my_name, '이고 나이는 ', my_age, '살이며 내년에는', next_age , ' 살이다')
+
+power = 2**10
+
+print('제곱연산은 ' , power)
+
+next_age = '30'
+
+print ('문자열의 연산은 이렇지', my_age + next_age )
